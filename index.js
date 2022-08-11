bf54962626a7815d44d78b5768686a89c3cf15de
cakejune <tyq444@gmail.com>
2022-08-11 14:43:15 -0400

    commit tree 22caa551ee75fef00d9fc7eda7afa3674d3eabb6

--- index.js
+++ index.js
@@ -1,1 +1,43 @@
-// Write your solution here!
+const cats = ["Milo", "Otis", "Garfield"];
+
+function beforeEach() {
+    cats.length = 0;
+    cats.push('Milo', 'Otis', 'Garfield');
+}
+
+function destructivelyAppendCat(name) {
+    cats.push(name);
+}
+
+function destructivelyPrependCat(name) {
+    cats.unshift(name);
+}
+
+function destructivelyRemoveLastCat() {
+    cats.pop();
+}
+
+function destructivelyRemoveFirstCat() {
+    cats.shift();
+}
+
+function appendCat(name) {
+    const cats2 = [...cats.slice(0,3), name];
+    return cats2;
+}
+
+function prependCat(name) {
+    const cats2 = [name, ...cats.slice(0,3)];
+    return cats2;
+}
+
+function removeLastCat() {
+    const cats2 = cats.slice(0,2);
+    return cats2;
+}
+
+function removeFirstCat() {
+    const cats2 = cats.slice(1);
+    return cats2;
+}
+    
